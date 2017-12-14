0cb1ad33431e320a74ebdeda9eed265b8c2b9c2f
Melody <Melody@Melodys-MacBook.local>
2017-12-14 13:53:28 -0700

    step 6

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const massive = require('massive');
+const controller = require('./controller');
 
 const app = express();
 app.use( bodyParser.json() );
@@ -13,11 +14,11 @@ massive(process.env.CONNECTION_STRING).then(db => {
 
     // db.new_planes().then(planes => {console.log(planes)}).catch(err => console.error(err))
     
-    db.get_planes().then(planes => {console.log(planes)}).catch(err => console.error(err))
+    // db.get_planes().then(planes => {console.log(planes)}).catch(err => console.error(err))
 
 }).catch(err => console.error(err))
 
-
+app.get('/api/planes',controller.getPlanes)
 
 const port = process.env.PORT || 3000
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
