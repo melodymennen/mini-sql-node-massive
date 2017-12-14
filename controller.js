8156fe9d2ac37edfa5208f19a34015f7938614db
Melody <Melody@Melodys-MacBook.local>
2017-12-14 14:10:32 -0700

    step 7

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
 module.exports = {
-    getPlanes(req,res,next) => {
+    getPlanes: (req,res,next) => {
         const db = req.app.get('db')
 
-        db.get_planes().then(planes => { res.status(200).send(planes); })
+        db.get_planes([25]).then(planes => { res.status(200).send(planes); })
         .catch( err => {
           console.log(err);
           res.status(500).send(err);
