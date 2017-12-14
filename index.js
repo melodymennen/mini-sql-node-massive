4526d9f2f1ac17f1fee134377272d4b4168900c5
Melody <Melody@Melodys-MacBook.local>
2017-12-14 12:19:49 -0700

    step 3

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
 
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db', db)
+}).catch(err => console.error(err))
+
 const port = process.env.PORT || 3000
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
 
